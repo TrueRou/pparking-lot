a013c60a1027a057018fb80e5e5394c15b8180f7
Usagi no Niku <chenbohan911@163.com>
2023-01-01 15:24:15 +0800

    first commit

---
+++ src/requests.js
@@ -0,0 +1,9 @@
+export const config = {
+    backend_entrypoint: "http://127.0.0.1:8000/",
+};
+
+export var requests = {
+    scores_global: config.backend_entrypoint + "scores/global",
+    scores_mode: config.backend_entrypoint + "scores/mode",
+    scores_player: config.backend_entrypoint + "scores/player",
+};
