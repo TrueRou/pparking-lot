72a1e8f94f6c91062cb094ce0eadb3630f007a68
arily <arily@arily.moe>
2023-01-05 09:39:17 +0900

    fmt

--- src/App.vue
+++ src/App.vue
@@ -212,32 +212,65 @@ export default {
     <el-container style="display: flex; flex-direction: column; min-height: 100vh">
       <el-header class="flush">
         <div style="display: flex; flex-wrap: wrap">
-          <el-menu :default-active="ruleset" @select="(v: Ruleset) => (ruleset = v)" class="el-menu-demo"
-            mode="horizontal" :ellipsis="false" ref="header-menu">
+          <el-menu
+            :default-active="ruleset"
+            @select="(v: Ruleset) => (ruleset = v)"
+            class="el-menu-demo"
+            mode="horizontal"
+            :ellipsis="false"
+            ref="header-menu"
+          >
             <el-menu-item :disabled="disableMenu" index="standard">standard</el-menu-item>
-            <el-menu-item :disabled="disableMenu || forbiddenRuleset(mode as Mode, 'relax')"
-              index="relax">relax</el-menu-item>
-            <el-menu-item :disabled="disableMenu || forbiddenRuleset(mode as Mode, 'autopilot')"
-              index="autopilot">autopilot</el-menu-item>
+            <el-menu-item
+              :disabled="disableMenu || forbiddenRuleset(mode as Mode, 'relax')"
+              index="relax"
+              >relax</el-menu-item
+            >
+            <el-menu-item
+              :disabled="disableMenu || forbiddenRuleset(mode as Mode, 'autopilot')"
+              index="autopilot"
+              >autopilot</el-menu-item
+            >
           </el-menu>
-          <div style="border-bottom: 1px solid var(--el-border-color); flex-grow: 1"></div>
-          <el-menu :default-active="mode" @select="(v: Mode) => (mode = v)" mode="horizontal" :ellipsis="false"
-            ref="header-menu">
+          <div
+            style="border-bottom: 1px solid var(--el-border-color); flex-grow: 1"
+          ></div>
+          <el-menu
+            :default-active="mode"
+            @select="(v: Mode) => (mode = v)"
+            mode="horizontal"
+            :ellipsis="false"
+            ref="header-menu"
+          >
             <el-menu-item :disabled="disableMenu" index="osu">osu</el-menu-item>
-            <el-menu-item :disabled="disableMenu || forbiddenMode(ruleset as Ruleset, 'taiko')"
-              index="taiko">taiko</el-menu-item>
-            <el-menu-item :disabled="disableMenu || forbiddenMode(ruleset as Ruleset, 'fruits')"
-              index="ctb">ctb</el-menu-item>
-            <el-menu-item :disabled="disableMenu || forbiddenMode(ruleset as Ruleset, 'mania')"
-              index="mania">mania</el-menu-item>
+            <el-menu-item
+              :disabled="disableMenu || forbiddenMode(ruleset as Ruleset, 'taiko')"
+              index="taiko"
+              >taiko</el-menu-item
+            >
+            <el-menu-item
+              :disabled="disableMenu || forbiddenMode(ruleset as Ruleset, 'fruits')"
+              index="ctb"
+              >ctb</el-menu-item
+            >
+            <el-menu-item
+              :disabled="disableMenu || forbiddenMode(ruleset as Ruleset, 'mania')"
+              index="mania"
+              >mania</el-menu-item
+            >
           </el-menu>
         </div>
       </el-header>
       <el-container class="flex-grow: 1">
         <el-aside width="fit-content">
           <div style="display: flex; flex-direction: column; height: 100%">
-            <el-menu :default-active="filter" @select="(v: 'all' | 'user') => (filter = v)" class="el-menu-demo" :ellipsis="false"
-              ref="header-menu">
+            <el-menu
+              :default-active="filter"
+              @select="(v: 'all' | 'user') => (filter = v)"
+              class="el-menu-demo"
+              :ellipsis="false"
+              ref="header-menu"
+            >
               <el-menu-item :disabled="disableMenu" index="all">Show all</el-menu-item>
               <el-menu-item :disabled="disableMenu" index="user">
                 <template v-if="filter === 'user'"> Type in user id below </template>
@@ -256,25 +289,39 @@ export default {
           </div>
         </el-aside>
         <el-main style="--el-main-padding: 0">
-          <el-table :data="tableData" style="height: 100%" :default-sort="{ prop: 'new_pp', order: 'descending' }">
+          <el-table
+            :data="tableData"
+            style="height: 100%"
+            :default-sort="{ prop: 'new_pp', order: 'descending' }"
+          >
             <el-table-column header-align="center" label="Score Information">
               <el-table-column prop="beatmap.title" label="Beatmap" fixed width="180">
                 <template #default="scope">
-                  <el-tooltip effect="dark" :content="'' + scope.row.beatmap.id" placement="top-start">
+                  <el-tooltip
+                    effect="dark"
+                    :content="'' + scope.row.beatmap.id"
+                    placement="top-start"
+                  >
                     <div>
-                      <el-image style="width: 160px; height: 40px" :src="
-                        'https://cdn.sayobot.cn:25225/beatmaps/' +
-                        scope.row.beatmap.set_id +
-                        '/covers/cover.jpg'
-                      " fit="cover" />
+                      <el-image
+                        style="width: 160px; height: 40px"
+                        :src="
+                          'https://cdn.sayobot.cn:25225/beatmaps/' +
+                          scope.row.beatmap.set_id +
+                          '/covers/cover.jpg'
+                        "
+                        fit="cover"
+                      />
                       <div>
-                        <span style="
+                        <span
+                          style="
                             font-size: 8px;
                             font-weight: bold;
                             overflow: hidden;
                             white-space: nowrap;
                             text-overflow: ellipsis;
-                          ">
+                          "
+                        >
                           {{ scope.row.beatmap.title }}
                         </span>
                         <br />
@@ -287,35 +334,71 @@ export default {
                 </template>
               </el-table-column>
               <el-table-column sortable prop="score.acc" label="Acc" width="80" />
-              <el-table-column sortable prop="score.nmiss" label="Miss" width="80"
-                label-class-name="small-table-column" />
-              <el-table-column sortable sort-by="performance.difficulty_attributes.stars" label="Stars" width="80"
-                label-class-name="small-table-column">
+              <el-table-column
+                sortable
+                prop="score.nmiss"
+                label="Miss"
+                width="80"
+                label-class-name="small-table-column"
+              />
+              <el-table-column
+                sortable
+                sort-by="performance.difficulty_attributes.stars"
+                label="Stars"
+                width="80"
+                label-class-name="small-table-column"
+              >
                 <template #default="scope">
                   <span style="font-size: 14px">{{
                     scope.row.performance.difficulty_attributes.stars.toFixed(2)
                   }}</span>
                 </template>
               </el-table-column>
-              <el-table-column sortable prop="score.mods_str" label="Mods" width="120"
-                label-class-name="small-table-column" />
+              <el-table-column
+                sortable
+                prop="score.mods_str"
+                label="Mods"
+                width="120"
+                label-class-name="small-table-column"
+              />
             </el-table-column>
             <el-table-column header-align="center" label="Performance">
-              <el-table-column sortable prop="performance.old_pp" label="Old" width="100" />
-              <el-table-column sortable prop="performance.new_pp" label="New" width="100" />
-              <el-table-column sortable label="Delta" width="120" :sort-method="sortPPChange">
+              <el-table-column
+                sortable
+                prop="performance.old_pp"
+                label="Old"
+                width="100"
+              />
+              <el-table-column
+                sortable
+                prop="performance.new_pp"
+                label="New"
+                width="100"
+              />
+              <el-table-column
+                sortable
+                label="Delta"
+                width="120"
+                :sort-method="sortPPChange"
+              >
                 <template #default="scope">
                   <span style="font-size: 14px">
                     {{
-                    (
-  scope.row.performance.new_pp - scope.row.performance.old_pp
-).toFixed(2)
+                      (
+                        scope.row.performance.new_pp - scope.row.performance.old_pp
+                      ).toFixed(2)
                     }}
                   </span>
                 </template>
               </el-table-column>
-              <el-table-column v-if="rxForm" sort-by="performance.performance_vn.pp" label="Vanilla"
-                label-class-name="small-table-column" sortable width="100">
+              <el-table-column
+                v-if="rxForm"
+                sort-by="performance.performance_vn.pp"
+                label="Vanilla"
+                label-class-name="small-table-column"
+                sortable
+                width="100"
+              >
                 <template #default="scope">
                   <span style="font-size: 14px">{{
                     scope.row.performance.performance_vn.pp.toFixed(2)
@@ -324,47 +407,83 @@ export default {
               </el-table-column>
             </el-table-column>
             <el-table-column header-align="center" label="Attributes">
-              <el-table-column sortable label="PPAim" width="120" :sort-method="sortPerformanceAim">
+              <el-table-column
+                sortable
+                label="PPAim"
+                width="120"
+                :sort-method="sortPerformanceAim"
+              >
                 <template #default="scope">
                   <span style="font-size: 14px">{{
                     formatPerformance(scope.row.performance, "pp_aim")
                   }}</span>
                 </template>
               </el-table-column>
-              <el-table-column sortable label="PPAcc" width="120" :sort-method="sortPerformanceAcc">
+              <el-table-column
+                sortable
+                label="PPAcc"
+                width="120"
+                :sort-method="sortPerformanceAcc"
+              >
                 <template #default="scope">
                   <span style="font-size: 14px">{{
                     formatPerformance(scope.row.performance, "pp_acc")
                   }}</span>
                 </template>
               </el-table-column>
-              <el-table-column sortable label="PPSpd" v-if="!rxForm" width="120" :sort-method="sortPerformanceSpeed">
+              <el-table-column
+                sortable
+                label="PPSpd"
+                v-if="!rxForm"
+                width="120"
+                :sort-method="sortPerformanceSpeed"
+              >
                 <template #default="scope">
                   <span style="font-size: 14px">{{
                     formatPerformance(scope.row.performance, "pp_speed")
                   }}</span>
                 </template>
               </el-table-column>
-              <el-table-column sortable label="EMC" width="120"
-                sort-by="performance.performance_attributes.effective_miss_count">
+              <el-table-column
+                sortable
+                label="EMC"
+                width="120"
+                sort-by="performance.performance_attributes.effective_miss_count"
+              >
                 <template #default="scope">
                   <span style="font-size: 14px">{{
                     getValueSafe(
                       scope.row.performance.performance_attributes.effective_miss_count
-                                      )
+                    )
                   }}</span>
                 </template>
               </el-table-column>
             </el-table-column>
-            <el-table-column header-align="center" v-if="rxForm" label="Attributes Vanilla">
-              <el-table-column v-if="rxForm" sortable label="PPAim" width="120" :sort-method="sortPerformanceAimVN">
+            <el-table-column
+              header-align="center"
+              v-if="rxForm"
+              label="Attributes Vanilla"
+            >
+              <el-table-column
+                v-if="rxForm"
+                sortable
+                label="PPAim"
+                width="120"
+                :sort-method="sortPerformanceAimVN"
+              >
                 <template #default="scope">
                   <span style="font-size: 14px">{{
                     formatPerformanceVN(scope.row.performance, "pp_aim")
                   }}</span>
                 </template>
               </el-table-column>
-              <el-table-column v-if="rxForm" sortable label="PPSpd" width="120" :sort-method="sortPerformanceSpeedVN">
+              <el-table-column
+                v-if="rxForm"
+                sortable
+                label="PPSpd"
+                width="120"
+                :sort-method="sortPerformanceSpeedVN"
+              >
                 <template #default="scope">
                   <span style="font-size: 14px">{{
                     formatPerformanceVN(scope.row.performance, "pp_speed")
@@ -374,14 +493,22 @@ export default {
             </el-table-column>
             <el-table-column align="center" label="Action" header-align="center">
               <template #default="scope">
-                <el-button @click="showStrains(scope.row.performance.id)">Analyze Strains</el-button>
+                <el-button @click="showStrains(scope.row.performance.id)"
+                  >Analyze Strains</el-button
+                >
               </template>
             </el-table-column>
           </el-table>
         </el-main>
       </el-container>
     </el-container>
-    <el-dialog v-if="rxForm" width="850" v-model="dialogStrains" title="Strains" style="margin-top: 50px">
+    <el-dialog
+      v-if="rxForm"
+      width="850"
+      v-model="dialogStrains"
+      title="Strains"
+      style="margin-top: 50px"
+    >
       <el-table max-height="720" :data="strainsData">
         <el-table-column sortable property="object" label="No." width="80" />
         <el-table-column header-align="center" label="Aim">
@@ -396,7 +523,13 @@ export default {
         </el-table-column>
       </el-table>
     </el-dialog>
-    <el-dialog v-if="!rxForm" width="360" v-model="dialogStrains" title="Strains" style="margin-top: 50px">
+    <el-dialog
+      v-if="!rxForm"
+      width="360"
+      v-model="dialogStrains"
+      title="Strains"
+      style="margin-top: 50px"
+    >
       <el-table max-height="720" :data="strainsData">
         <el-table-column sortable property="object" label="No." width="80" />
         <el-table-column sortable property="aim" label="Aim" width="120" />
